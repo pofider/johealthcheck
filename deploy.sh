cbb6dda6621475e1c57a812cf7559df703a7c8ed
pofider <jan.blaha@hotmail.com>
2018-07-26 16:13:13 +0200

    rename to johealthcheck

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t jo-health-check .
-docker tag jo-health-check pofider/jo-health-check:$TRAVIS_TAG
-docker push pofider/jo-health-check
+docker build -t johealthcheck .
+docker tag johealthcheck pofider/johealthcheck:$TRAVIS_TAG
+docker push pofider/johealthcheck
 
 git clone https://github.com/pofider/kubernetes.git
 cd kubernetes
 chmod +x push.sh
-./push.sh "jo" "pofider/jo-health-check"
+./push.sh "johealthcheck" "pofider/johealthcheck"
